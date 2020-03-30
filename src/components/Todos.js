f2f67264318fc6853a7ec71bbea6a0dc8ab2af2e
JQ <2016quezadaj@gmail.com>
2020-03-29 17:38:57 -0700

    Started todo repo with React

---
+++ src/components/Todos.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Todos extends Component {
+  render() {
+    return  (
+        <div className="App">
+        
+        <h1>Todo List</h1>
+        
+        </div>
+        );
+    }
+}
+
+export default Todos;
